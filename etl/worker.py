cec6991fd404a54b140d974f38ce6a628ea1c310
Terry Weymouth <weymouth@umich.edu>
2018-04-06 17:03:13 -0400

    first commit

---
+++ etl/worker.py
@@ -0,0 +1,97 @@
+import os
+import logging
+from materials_commons.etl.input_spreadsheet import BuildProjectExperiment
+from .globus_service import MaterialsCommonsGlobusInterface
+from .VerifySetup import VerifySetup
+import configparser
+
+
+GLOBUS_QUEUE = 'elt:globus-transfer'
+PROCESS_QUEUE = 'etl:build-experiment'
+
+
+class Faktroy:
+    def __init__(self, project_id):
+        self.project_id = project_id
+        self.status = None
+        self.queue = None
+        self.extras = {}
+
+
+class Worker:
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.log = logging.getLogger(__name__ + "." + self.__class__.__name__)
+        user_endoint_config_file_path = os.path.join('.globus_test', 'endpoint.ini')
+        config_file_locaton_for_user_endpoint = os.path.join(os.path.expanduser("~"), user_endoint_config_file_path)
+        config = configparser.ConfigParser()
+        config.read(str(config_file_locaton_for_user_endpoint))
+        self.worker_base_path = config['worker']['base_path']
+
+    def run_with(self, project_id, experiment_name, experiment_description,
+                 globus_endpoint, endpoint_path, request_uuid,
+                 excel_file_relative_path, data_dir_relitive_path):
+
+        base_path = self.worker_base_path
+        transfer_base_path = "{}/transfer-{}".format(base_path, request_uuid)
+        excel_file_path = "{}/{}".format(transfer_base_path, excel_file_relative_path)
+        data_file_path = "{}/{}".format(transfer_base_path, data_dir_relitive_path)
+        self.log.info("excel_file_path = " + excel_file_path)
+        self.log.info("data_file_path = " + data_file_path)
+        results = self.verify_preconditions(
+            project_id, globus_endpoint, endpoint_path, transfer_base_path,
+            excel_file_relative_path, data_dir_relitive_path
+        )
+        if not results['status'] == 'SUCCEEDED':
+            self.log.error("Preconditions for transfer failed...")
+            for key in results:
+                self.log.error(" Failure: " + key + " :: " + results[key])
+            return
+
+        # tracking_record = self.initialize_tracking(project_id, experiment_name, experiment_description,
+        #          globus_endpoint, endpoint_path, request_uuid,
+        #          excel_file_relative_path, data_dir_relitive_path)
+        #
+        # self.submit_globus_transfer(project_id, globus_endpoint, endpoint_path)
+        else:
+            results = self.globus_transfer(project_id, globus_endpoint, endpoint_path)
+            self.log.info(results)
+            if results['status'] == 'SUCCEEDED':
+                self.build_experiment(project_id, experiment_name, experiment_description,
+                                      excel_file_path, data_file_path)
+
+    def verify_preconditions(self, project_id,
+                 globus_endpoint, endpoint_path, base_path,
+                 excel_file_relative_path, data_dir_relitive_path):
+        web_service = MaterialsCommonsGlobusInterface(self.user_id)
+        checker = VerifySetup(web_service, project_id,
+                 globus_endpoint, endpoint_path, base_path,
+                 excel_file_relative_path, data_dir_relitive_path)
+        return checker.status()
+
+    def globus_transfer(self, project_id, globus_endpoint, endpoint_path):
+        web_service = MaterialsCommonsGlobusInterface(self.user_id)
+        self.log.info("set_transfer_client")
+        results = web_service.set_transfer_client()
+        if results['status'] == 'error':
+            return results
+
+        self.log.info("stage_upload_files")
+        results = web_service.stage_upload_files(project_id, globus_endpoint, endpoint_path)
+        self.log.info("results of staging: ", results)
+        task_id = results['task_id']
+        poll = True
+        while poll:
+            results = web_service.get_task_status(task_id)
+            poll = (results['status'] == 'ACTIVE')
+        self.log.info(results)
+        return results
+
+    @staticmethod
+    def build_experiment(project_id, experiment_name, experiment_description,
+                         excel_file_path, data_file_path):
+        builder = BuildProjectExperiment()
+        builder.set_rename_is_ok(True)
+        builder.preset_project_id(project_id)
+        builder.preset_experiment_name_description(experiment_name, experiment_description)
+        builder.build(excel_file_path, data_file_path)
---
+++ run_worker_once.py
@@ -0,0 +1,55 @@
+import os
+import logging
+import sys
+import configparser
+from etl.worker import Worker
+
+
+user_endoint_config_file_path = os.path.join('.globus_test', 'endpoint.ini')
+config_file_locaton_for_user_endpoint = os.path.join(os.path.expanduser("~"), user_endoint_config_file_path)
+
+
+def main():
+
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+
+    log = logging.getLogger('elt.run_worker_once')
+
+    config = configparser.ConfigParser()
+    config.read(str(config_file_locaton_for_user_endpoint))
+
+    user_id = "test@test.mc"
+    project_id = "4981e174-7aee-4729-b692-6134bd60dd9d"
+    experiment_name = "Test from excel"
+    experiment_description = "An experiment built via etl from test data"
+    globus_endpoint = config['test']['endpoint']
+    endpoint_path = config['test']['directory']
+    request_uuid = project_id
+    excel_file_relative_path = "input.xlsx"
+    data_dir_relitive_path = "data"
+
+    log.info("user_id = " + user_id)
+    log.info("project_id = " + project_id)
+    log.info("experiment_name = " + experiment_name)
+    log.info("experiment_description = " + experiment_description)
+    log.info("globus_endpoint = " + globus_endpoint)
+    log.info("endpoint_path = " + endpoint_path)
+    log.info("request_uuid = " + request_uuid)
+    log.info("excel_file_relative_path = " + excel_file_relative_path)
+    log.info("data_dir_relitive_path = " + data_dir_relitive_path)
+
+    worker = Worker(user_id)
+    worker.run_with(project_id, experiment_name, experiment_description,
+                    globus_endpoint, endpoint_path, request_uuid,
+                    excel_file_relative_path, data_dir_relitive_path)
+
+
+if __name__ == "__main__":
+    main()
